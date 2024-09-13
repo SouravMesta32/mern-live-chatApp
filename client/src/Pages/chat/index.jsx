89ae0ae91cc9070163d92d653ffe6496525a6a2c
sourav <souravmesta32@gmail.com>
2024-09-13 06:52:14 +0530

    created chat layout

--- client/src/Pages/chat/index.jsx
+++ client/src/Pages/chat/index.jsx
@@ -2,6 +2,9 @@ import { useAppStore } from "@/store/index"
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ChatContainer from "./components/chat-container";
+import ContactsContainer from "./components/contact-container";
+import EmptyChatContainer from "./components/empty-chat-container";
 
 const Chat = () => {
   const { userInfo } = useAppStore();
@@ -18,7 +21,11 @@ const Chat = () => {
   },[userInfo,navigate])
 
   return (
-    <div>Chat</div>
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactsContainer/>
+      <EmptyChatContainer/>
+      <ChatContainer/>
+    </div>
   )
   }
 
