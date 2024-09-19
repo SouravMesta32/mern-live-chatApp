ba2d4e193ba267d9abf2cb267f0502912543831e
sourav <souravmesta32@gmail.com>
2024-09-19 13:23:38 +0530

    Socket setup frontend [socket.io-client]

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,11 @@ import { useSonner } from 'sonner'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from "@/components/ui/sonner"
+import { SocketProvider } from './context/socketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <SocketProvider>
     <App />
     <Toaster closeButton classname="bg-red-500"/>
-  </>
+  </SocketProvider>
 )
