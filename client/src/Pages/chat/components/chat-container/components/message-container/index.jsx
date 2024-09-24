b83972bb18bc1f3df36a9e5e3e3de24bbcfd878e
sourav <souravmesta32@gmail.com>
2024-09-24 13:56:19 +0530

    created download funciton, which lets download files

--- client/src/Pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/Pages/chat/components/chat-container/components/message-container/index.jsx
@@ -56,7 +56,16 @@ const MessageContainer = () => {
     })
   }
 
-  const downloadFile = (file) =>{
+  const downloadFile = async (url) =>{
+    const response = await apiclient.get(`${HOST}/${url}`,{responseType:"blob"})
+    const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = urlBlob;
+    link.setAttribute("download",url.split("/").pop())
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(urlBlob)
 
   }
 
