735ce51162e664de8495b969dc7fcdc3178bebb9
sourav <souravmesta32@gmail.com>
2024-10-05 11:53:29 +0530

    modified chat contianer

--- client/src/Pages/chat/components/chat-container/index.jsx
+++ client/src/Pages/chat/components/chat-container/index.jsx
@@ -5,9 +5,9 @@ import MessageContainer from './components/message-container'
 
 const ChatContainer = () => {
   return (
-    <div className="fixed inset-0 flex flex-col bg-[#1c1d25] md:static md:flex-1">
+    <div className="fixed inset-0 flex flex-col bg-[#1c1d25] md:static md:flex-1 overflow-hidden">
       <ChatHeader />
-      <div className="flex-1 overflow-y-auto">
+      <div className="flex-1 overflow-y-auto overflow-x-hidden">
         <MessageContainer />
       </div>
       <div className="sticky bottom-0 w-full">
